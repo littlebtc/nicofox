f577e207eebaaeb39dbe644821e4ad636069e4eb
Hsiao-Ting Yu <sst.dreams@gmail.com>
2008-09-16 03:42:47 +0000

    Truly add data

---
+++ defaults/preferences/nicofox.js
@@ -0,0 +1,8 @@
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.nicofox.first_run", true);
+pref("extensions.nicofox.save_path", '');
+pref("extensions.nicofox.confirm_before_download", true);
+pref("extensions.nicofox.boon_comment", true);
+pref("extensions.nicofox.nicomonkey.enable", true);
+pref("extensions.nicofox.nicomonkey.toolbar", true);
+pref("extensions.nicofox.nicomonkey.player_killer", false);
