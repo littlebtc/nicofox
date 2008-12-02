446d41b451d321514d1a2b58672f5cf60be57984
Hsiao-Ting Yu <sst.dreams@gmail.com>
2008-12-02 21:30:42 +0000

    update pref

--- defaults/preferences/nicofox.js
+++ defaults/preferences/nicofox.js
@@ -4,9 +4,20 @@ pref("extensions.nicofox.save_path", '');
 pref("extensions.nicofox.confirm_before_download", true);
 pref("extensions.nicofox.boon_comment", true);
 pref("extensions.nicofox.nicowa_blocker", false);
+// New in 0.3
+//pref("extensions.nicofox.external_video_player", false);
+//pref("extensions.nicofox.external_video_player_path", '');
+//pref("extensions.nicofox.external_swf_player", false);
+//pref("extensions.nicofox.external_swf_player_path", '');
+
+// NicoMonkey
 pref("extensions.nicofox.nicomonkey.enable", true);
 pref("extensions.nicofox.nicomonkey.toolbar", true);
 pref("extensions.nicofox.nicomonkey.comment_helper", false);
 pref("extensions.nicofox.nicomonkey.player_killer", false);
+
+// New in 0.3
+pref("extensions.nicofox.nicomonkey.sound_converter", 'http://nicosound.anyap.info/sound/%1');
+
 // Easter Egg!
 pref("extensions.nicofox.tsundere", false);
