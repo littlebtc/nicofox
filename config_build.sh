c269adeb47e0d409d7f1670fcb95c30d0271c405
Hsiao-Ting Yu <sst.dreams@gmail.com>
2008-12-09 21:38:37 +0000

    fix config_build.sh

--- config_build.sh
+++ config_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Build config for build.sh
 APP_NAME="nicofox"
-CHROME_PROVIDERS="content locale skin"
+CHROME_PROVIDERS="content locale skin modules"
 CLEAN_UP=1
 ROOT_FILES=
 ROOT_DIRS="defaults components"
