814e587d22cd81ca55cbf42973c7f5fe45a06dda
Hsiao-Ting Yu <sst.dreams@gmail.com>
2008-12-09 21:39:57 +0000

    config_build.sh fix again...

--- config_build.sh
+++ config_build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Build config for build.sh
 APP_NAME="nicofox"
-CHROME_PROVIDERS="content locale skin modules"
+CHROME_PROVIDERS="content locale skin"
 CLEAN_UP=1
 ROOT_FILES=
-ROOT_DIRS="defaults components"
+ROOT_DIRS="defaults components modules"
 BEFORE_BUILD=
 AFTER_BUILD=
