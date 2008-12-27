f2c7e833470db81b888823222e14b8674ea4f5fe
Hsiao-Ting Yu <sst.dreams@gmail.com>
2008-12-27 11:14:12 +0000

    Add license.txt; fix ja locale

--- config_build.sh
+++ config_build.sh
@@ -3,7 +3,7 @@
 APP_NAME="nicofox"
 CHROME_PROVIDERS="content locale skin"
 CLEAN_UP=1
-ROOT_FILES=
+ROOT_FILES=license.txt
 ROOT_DIRS="defaults components modules player platform"
 BEFORE_BUILD=
 AFTER_BUILD=
