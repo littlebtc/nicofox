b501b6902b21e51a5d959b3fcb8c48cd14ac7f15
Hsiao-Ting Yu <sst.dreams@gmail.com>
2009-02-19 13:25:23 +0000

    Add download_helper.js, to prepare multiple sites support

--- modules/download_helper_nico.js
+++ modules/download_helper_nico.js
@@ -10,96 +10,10 @@ var prompts = Cc["@mozilla.org/embedcomp/prompt-service;1"]
 /* Multiple downloads helper, for simultaneously handle multiple persist without progress checking 
    XXX: file checks
 */
-function multipleDownloadsHelper() { }
-multipleDownloadsHelper.prototype = {
-  persists: [],
-  files: [],
-  adding: true,
-  download_count: 0,
-  doneCallback: function() {},
-  /* Add a download. NOTE: file should be a nsIFile, not a string!! */
-  addDownload: function(dl_url, referrer, post_string, file, bypass_cache, single_callback) {
-    this.download_count++;
-    if (referrer) {
-      var ref_uri = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService)
-                    .newURI(referrer, null, null);
-    } else {
-      var ref_uri = null;
-    }
-    var dl_uri = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService).newURI(dl_url, null, null);
-
-    /* POST header processing */
-    if (post_string) { 
-      var post_stream = Cc["@mozilla.org/io/string-input-stream;1"]
-                       .createInstance(Ci.nsIStringInputStream);
-      post_stream.setData(post_string, post_string.length); // NicoFox 0.3+ support Gecko/XULRunner 1.9+ only
-
-      var post_data = Cc["@mozilla.org/network/mime-input-stream;1"]
-                     .createInstance(Ci.nsIMIMEInputStream);
-  
-      post_data.addHeader("Content-Type", "application/x-www-form-urlencoded");
-      post_data.addContentLength = true;
-      post_data.setData(post_stream);
-    } else {
-      post_data = null;
-    }
-    var persist = Cc["@mozilla.org/embedding/browser/nsWebBrowserPersist;1"].createInstance(Ci.nsIWebBrowserPersist);
-    var flags =  Ci.nsIWebBrowserPersist.PERSIST_FLAGS_AUTODETECT_APPLY_CONVERSION |
-                 Ci.nsIWebBrowserPersist.PERSIST_FLAGS_REPLACE_EXISTING_FILES;
-    if (bypass_cache) {
-      flags = flags | Ci.nsIWebBrowserPersist.PERSIST_FLAGS_BYPASS_CACHE;
-    }  
-    persist.persistFlags = flags; 
-
-    if (!single_callback || typeof single_callback != 'function') {
-      single_callback = function() {};
-    } 
-    var _this = this;
-    persist.progressListener = {
-      callback: single_callback,
-      onStateChange: function (aWebProgress, aRequest, aStateFlags, aStatus) {
-        if (aStateFlags & 16) /* STATE_STOP = 16 */ {
-	  this.callback();
-	  nicofox.hitch(_this, 'completeDownload')();
-        }
-      },
-      onProgressChange: function (aWebProgress, aRequest,
-                                  aCurSelfProgress, aMaxSelfProgress,
-                                  aCurTotalProgress, aMaxTotalProgress) {},
-      onLocationChange: function (aWebProgress, aRequest, aLocation) {},
-      onStatusChange  : function (aWebProgress, aRequest, aStatus, aMessage) {},
-      onSecurityChange: function (aWebProgress, aRequest, aState) {},
-    };
-
-    persist.saveURI(dl_uri, null, ref_uri, post_data, null, file);
-    this.persists.push(persist);
-  },
-  goAhead: function() {
-    this.adding = false;
-    if (this.download_count == 0) {
-      this.finalize();
-    }
-  },
-  completeDownload: function() {
-    this.download_count--;
-    if (this.download_count == 0 && !this.adding) {
-      this.finalize();
-    }
-  },
-  finalize: function() {
-    this.doneCallback();
-  },
-  cancelAll: function() {
-   this.persists.forEach(function (element, index, array) {
-     if (element) {
-       array[index].cancelSave(); 
-     }
-   });
-  }
-}
 if (!nicofox) { var nicofox = {}; }
 if (!nicofox.download) { nicofox.download = {}; }
 if (!nicofox.download.helper) { nicofox.download.helper = {}; }
+
 nicofox.download.helper.nico = function() { };
 nicofox.download.helper.nico.prototype = {
   download_comment: false,
@@ -372,7 +286,7 @@ nicofox.download.helper.nico.prototype = {
       var owner_post_header = 
       '<thread click_revision="0" fork="1" user_id="'+params.user_id+'" res_from="-1000" version="20061206" thread="'+params.thread_id+'"/>';
     }
-    this.download_helper = new multipleDownloadsHelper();
+    this.download_helper = new nicofox.download.helper.multiple();
     this.download_helper.doneCallback = nicofox.hitch(this, 'callback', 'completed', {});
     this.download_helper.addDownload(params.ms, null , post_header, this.ms_file, true, nicofox.hitch(this, 'processNicoComment', params));
     if(this.uploader_comment) {
