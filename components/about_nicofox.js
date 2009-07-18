ae23c00421fa131471c285b21e9c3b958654d8e2
Hsiao-Ting Yu <sst.dreams@gmail.com>
2009-07-18 19:27:55 +0000

    update components to make about:nicofox URL work

---
+++ components/about_nicofox.js
@@ -0,0 +1,31 @@
+/* Add about:nicofox to Firefox
+ Original from https://developer.mozilla.org/En/Custom_about:_URLs */
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+
+function AboutNicoFox() { }
+AboutNicoFox.prototype = {
+  classDescription: "about:nicofox",
+  contractID: "@mozilla.org/network/protocol/about;1?what=nicofox",
+  classID: Components.ID("{af1db570-70a8-11de-8a39-0800200c9a66}"),
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIAboutModule]),
+  
+  getURIFlags: function(aURI) {
+    return Ci.nsIAboutModule.ALLOW_SCRIPT;
+  },
+  
+  newChannel: function(aURI) {
+    let ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
+    let channel = ios.newChannel("chrome://nicofox/content/about_nicofox/index.xhtml",
+                                 null, null);
+    channel.originalURI = aURI;
+    return channel;
+  }
+};
+
+function NSGetModule(compMgr, fileSpec)
+  XPCOMUtils.generateModule([AboutNicoFox]);
+
