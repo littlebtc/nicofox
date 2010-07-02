95fb88efe5fba21dccff57cdbf17e607ba53ac34
Hsiao-Ting Yu <sst.dreams@gmail.com>
2010-07-02 16:07:07 +0000

    NicoWatcher cleanup, avoid frequent pref reading, regex speed up, use XPCOMUtils.jsm

--- components/nicowatcher.js
+++ components/nicowatcher.js
@@ -1,112 +1,89 @@
+/* vim: sw=2 ts=2 sts=2 et filetype=javascript 
+ *
+ * Use Content Policy to watch the download manager URL (?smilefox=get) or Nicowa (Used for Nicowa blocker)
+ * Code originally referenced from IE tab & Greasemonkey
+ */
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const CONTRACT_ID = "@littleb.tc/nicowatcher;1";
 
-const nsISupports = Components.interfaces.nsISupports;
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 
-const CLASS_ID = Components.ID("62ca6b17-975f-4c70-b497-33146a16c797");
-const CLASS_NAME = "";
-const CONTRACT_ID = "@littlebtc.twbbs.org/nicowatcher;1";
-
-
-var Nicowatcher = {
-
-    /* By following is the code stolen and modified from IE Tab and Greasemonkey (Expat License) */
-
-  // nsIContentPolicy interface implementation
-  shouldLoad: function(contentType, contentLocation, requestOrigin, requestingNode, mimeTypeGuess, extra)
-	{
-		var prefs = Components.classes["@mozilla.org/preferences-service;1"]
-		            .getService(Components.interfaces.nsIPrefService);
-		prefs = prefs.getBranch("extensions.nicofox.");
-
-		if (contentType == Components.interfaces.nsIContentPolicy.TYPE_DOCUMENT)
-		{
-			url = contentLocation.spec;
-			/* XXX: Why not intergrate with nicofox.parser in modules/urlparser.js ? */
-			if (url.match(/^http:\/\/(www|tw|de|es)\.nicovideo\.jp\/(watch|mylist)\/[0-9a-zA-Z]+\?smilefox\=get$/)
-			//|| url.match(/^http:\/\/tw\.keytalks\.com\/watch\/?\?.*\&smilefox\=get$/)
-			//|| url.match(/^http:\/\/(www\.)?parasitestage\.net\/Page\/MediaView\.aspx\?ID\=[0-9]+\&smilefox\=get$/)
-			)
-			{
-			
-			var winWat = Components.classes["@mozilla.org/embedcomp/window-watcher;1"]
-		    .getService(Components.interfaces.nsIWindowWatcher);
-
-			if (winWat.activeWindow && winWat.activeWindow.nicofox_ui && winWat.activeWindow.nicofox_ui.overlay) {
-			  /* We have no setTimeout(), so... */
-			  var timer_event = { notify: function () {winWat.activeWindow.nicofox_ui.overlay.goDownload(url);} }
-			  var timer = Components.classes["@mozilla.org/timer;1"]
-			                        .createInstance(Components.interfaces.nsITimer);
- 			  timer.initWithCallback(timer_event, 10, Components.interfaces.nsITimer.TYPE_ONE_SHOT);
-			}
-			
-			return Components.interfaces.nsIContentPolicy.REJECT_REQUEST;
-			}
-		}
-		/* Block NicoWa if needed */
-		else if (contentType == Components.interfaces.nsIContentPolicy.TYPE_OBJECT_SUBREQUEST)
-		{
-			url = contentLocation.spec;
-			if(prefs.getBoolPref('nicowa_blocker') && url.indexOf('http://flapi.nicovideo.jp/api/getmarqueev3') == 0 )
-			{
-				return Components.interfaces.nsIContentPolicy.REJECT_REQUEST;
-			}
-		}
-   return Components.interfaces.nsIContentPolicy.ACCEPT;
-	},
-  // this is now for urls that directly load media, and meta-refreshes (before activation)
-  shouldProcess: function(contentType, contentLocation, requestOrigin, requestingNode, mimeType, extra) {
-    return (Components.interfaces.nsIContentPolicy.ACCEPT);
-  },
-
+function NicoWatcher() {
+  this.wrappedJSObject = this;
 }
 
-//=================================================
-// Note: You probably don't want to edit anything
-// below this unless you know what you're doing.
-//
-// Factory
-var NicowatcherFactory = {
-  createInstance: function (aOuter, aIID)
-  {
-    if (aOuter != null)
-      throw Components.results.NS_ERROR_NO_AGGREGATION;
-    return Nicowatcher;
+NicoWatcher.prototype = {
+  classDescription: "URL Loading Watcher for NicoFox",
+  classID: Components.ID("62ca6b17-975f-4c70-b497-33146a16c797"),
+  contractID: CONTRACT_ID,
+  _xpcom_categories: [
+    { category: "content-policy" },
+    { category: "app-startup", service: true }
+  ],
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIContentPolicy, Ci.nsIObserver, Ci.nsIWeakReference]),
+  _nicowaBlocker: null,
+  /* Implements nsIContentPolicy */
+  shouldLoad: function(contentType, contentLocation, requestOrigin, requestingNode, mimeTypeGuess, extra) {
+    if (contentType == Ci.nsIContentPolicy.TYPE_DOCUMENT) {
+      var url = contentLocation.spec;
+      /* Simplify the filter code to make it faster (don't check Nicovideo URL here) */
+      if (url.search(/\?smilefox\=get$/) != -1) {
+        var winWat = Cc["@mozilla.org/embedcomp/window-watcher;1"].getService(Ci.nsIWindowWatcher);
+        if (winWat.activeWindow && winWat.activeWindow.nicofox_ui && winWat.activeWindow.nicofox_ui.overlay) {
+          /* We have no setTimeout(), so... */
+          var timer_event = { notify: function () {winWat.activeWindow.nicofox_ui.overlay.goDownload(url);} }
+          var timer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
+          timer.initWithCallback(timer_event, 10, Ci.nsITimer.TYPE_ONE_SHOT);
+        }
+        
+        return Ci.nsIContentPolicy.REJECT_REQUEST;
+      }
+    /* Block NicoWa if needed */
+    } else if (contentType == Ci.nsIContentPolicy.TYPE_OBJECT_SUBREQUEST) {
+      var url = contentLocation.spec;
+      if(this._nicowaBlocker && url.indexOf('http://flapi.nicovideo.jp/api/getmarqueev3') == 0 ) {
+        return Ci.nsIContentPolicy.REJECT_REQUEST;
+      }
+    }
+   return Ci.nsIContentPolicy.ACCEPT;
+  },
+  // this is now for urls that directly load media, and meta-refreshes (before activation)
+  shouldProcess: function(contentType, contentLocation, requestOrigin, requestingNode, mimeType, extra) {
+    return Ci.nsIContentPolicy.ACCEPT;
+  },
+  /* Implements nsIObserver */
+  observe: function(subject, topic, data) {
+    /* In app-startup, profiles are unavailable */
+    if (topic == "app-startup") {
+      var obs = Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService);
+      obs.addObserver(this, "final-ui-startup", false);
+      obs.addObserver(this, "quit-application", false);
+      return;
+    }
+    /* In Startup or pref changed, cache the value of nicowa_blocker (avoid pref access in shouldLoad) */
+    Components.utils.import("resource://nicofox/Core.jsm");
+    switch(topic) {
+      case "final-ui-startup":
+      var obs = Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService);
+      obs.removeObserver(this, "final-ui-startup");
+      Core.prefs.addObserver("nicowa_blocker", this, false);
+      this._nicowaBlocker = Core.prefs.getBoolPref("nicowa_blocker");
+      break;
+      
+      case "nsPref:changed":
+      this._nicowaBlocker = Core.prefs.getBoolPref("nicowa_blocker");
+      break;
+      
+      case "quit-application":
+      Components.utils.import("resource://nicofox/Services.jsm");
+      Services.obs.removeObserver(this, "quit-application")
+      Core.prefs.removeObserver("nicowa_blocker", this);
+      break;
+    }
   }
-};
+}
 
-// Module
-var NicowatcherModule = {
-  registerSelf: function(aCompMgr, aFileSpec, aLocation, aType)
-  {
-    aCompMgr = aCompMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
-    aCompMgr.registerFactoryLocation(CLASS_ID, CLASS_NAME, CONTRACT_ID, aFileSpec, aLocation, aType);
-
-    var catMgr = Components.classes["@mozilla.org/categorymanager;1"].getService(Components.interfaces.nsICategoryManager);
-    catMgr.addCategoryEntry("content-policy", CONTRACT_ID, CONTRACT_ID, true, true);
-  },
-
-  unregisterSelf: function(aCompMgr, aLocation, aType)
-  {
-    aCompMgr = aCompMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
-    aCompMgr.unregisterFactoryLocation(CLASS_ID, aLocation);        
-
-    var catMgr = Components.classes["@mozilla.org/categorymanager;1"].getService(Components.interfaces.nsICategoryManager);
-    catMgr.deleteCategoryEntry("content-policy", CONTRACT_ID, true);
-  },
-  
-  getClassObject: function(aCompMgr, aCID, aIID)
-  {
-    if (!aIID.equals(Components.interfaces.nsIFactory))
-      throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
-
-    if (aCID.equals(CLASS_ID))
-      return NicowatcherFactory;
-
-    throw Components.results.NS_ERROR_NO_INTERFACE;
-  },
-
-  canUnload: function(aCompMgr) { return true; }
-};
-
-//module initialization
-function NSGetModule(aCompMgr, aFileSpec) { return NicowatcherModule; }
-
+function NSGetModule(compMgr, fileSpec) {
+  return XPCOMUtils.generateModule([NicoWatcher]);
+}
