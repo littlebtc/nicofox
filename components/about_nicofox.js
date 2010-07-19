9cbcd99a8df51001e8c1ca382960dada3bede325
Hsiao-Ting Yu <sst.dreams@gmail.com>
2010-07-19 15:05:11 +0000

    rename content/about_nicofox to content/inpage to prepare more inpage content

--- components/about_nicofox.js
+++ components/about_nicofox.js
@@ -19,7 +19,7 @@ AboutNicoFox.prototype = {
   
   newChannel: function(aURI) {
     let ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
-    let channel = ios.newChannel("chrome://nicofox/content/about_nicofox/index.xhtml",
+    let channel = ios.newChannel("chrome://nicofox/content/inpage/index.xhtml",
                                  null, null);
     channel.originalURI = aURI;
     return channel;
